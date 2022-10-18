c03fae73487167253155a4ace2e798a74f3f472d
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-10-18 21:20:17 +0800

    hw3 code done

--- Assignment3_Kalman_Filter/kalman_filter.py
+++ Assignment3_Kalman_Filter/kalman_filter.py
@@ -7,19 +7,40 @@ class KalmanFilter():
         # Transition matrix
         self.A = np.identity(3)
         self.B = np.identity(3)
+        # [1.0, 0.0, 0.0]
+        # [0.0, 1.0, 0.0]
+        # [0.0, 0.0, 1.0]
+
         # Error matrix
         self.P = np.identity(3) * 1
+        # [1, 0, 0]
+        # [0, 1, 0]
+        # [0, 0, 1]
+
         # Observation matrix
         self.H = np.array([[1, 0, 0],
                            [0, 1, 0]])
+        
+        self.n = self.A.shape[1]
+        
         # State transition error covariance
-        self.Q = None
+        self.Q = np.eye(self.n)
         # Measurement error
-        self.R = None
+        self.R = np.eye(self.n)
 
     def predict(self, u):
-        raise NotImplementedError
+        self.x = np.dot(self.A, self.x) + np.dot(self.B, u)
+        self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.R
+        # raise NotImplementedError
 
     def update(self, z):
-        raise NotImplementedError
+        
+        S = self.R + np.dot(self.H.T, np.dot(self.H, self.P))
+        K = np.dot(np.linalg.inv(S), np.dot(self.P, self.H.T))
+        y = z - np.dot(self.H, self.x)
+        self.x = self.x + np.dot(K, y)
+
+        I = np.eye(self.n)
+        self.P = np.dot(I - np.dot(K, self.H), self.P)
         return self.x, self.P
+        # raise NotImplementedError
